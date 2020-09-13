56eed063ebdd963dfaf13f1687ee714259d9c413
Jennifer Jacobson <jenniferjacobson@jennifers-air.lan>
2020-09-13 14:15:26 -0500

    part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Here are some photos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://previews.123rf.com/images/jungleoutthere/jungleoutthere1808/jungleoutthere180800006/105747423-cute-and-funny-taco-cat-vector-illustration-.jpg';
   image3 = '';
 
   constructor() { }
